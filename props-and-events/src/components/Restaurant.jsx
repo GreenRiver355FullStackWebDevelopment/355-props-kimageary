6df9859b36a176e50f947d327604a4a5752f41c2
Kimberly Mageary <kimberlymageary@gmail.com>
2025-04-13 20:35:42 -0700

    Finished LP 2

--- props-and-events/src/components/Restaurant.jsx
+++ props-and-events/src/components/Restaurant.jsx
@@ -1,8 +1,15 @@
-function Restaurant() {
+function Restaurant(props) {
 
   return (
     <div className="restaurant">
-  
+      <img src={props.restaurant.image} alt={props.name} />
+      <h1>{props.restaurant.name}</h1>
+      <ul>
+        <li>{props.restaurant.address}</li>
+        <li>{props.restaurant.phone}</li>
+        <li>{props.restaurant.cuisine}</li>
+        <li>⭐ {props.restaurant.rating}</li>
+      </ul>
     </div>
   );
 }
